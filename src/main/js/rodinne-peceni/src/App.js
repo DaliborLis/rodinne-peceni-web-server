11d85b90e663d6eaec01723c565e80bccaf8f08a
Dalibor Lis <dalibor.lis@oracle.com>
2024-02-24 20:26:38 +0100

    Add React App

---
+++ src/main/js/rodinne-peceni/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Menu from './Menu';
+
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
+  return (
+    <>
+      {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+      <Toast show={show} onClose={() => toggleShow(false)}>
+        <Toast.Header>
+          <strong className="mr-auto">React-Bootstrap</strong>
+        </Toast.Header>
+        <Toast.Body>{children}</Toast.Body>
+      </Toast>
+    </>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Menu />
+      <Container className="p-3">
+        <Container className="p-5 mb-4 bg-light rounded-3">
+          <h1 className="header">Welcome To React-Bootstrap</h1>
+          <ExampleToast>
+            We now have Toasts
+            <span role="img" aria-label="tada">
+              🎉
+            </span>
+          </ExampleToast>
+        </Container>
+      </Container>
+    </>
+  );
+}
+
+export default App;
