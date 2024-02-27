f61bc71e7f3c45cd41a53e98241b530371156904
Dalibor Lis <dalibor.lis@oracle.com>
2024-02-27 19:50:59 +0100

    Add categories

--- src/main/js/rodinne-peceni/src/components/App.js
+++ src/main/js/rodinne-peceni/src/components/App.js
@@ -2,13 +2,16 @@ import Menu from "./Menu";
 import Carousel from 'react-bootstrap/Carousel';
 import Image from 'react-bootstrap/Image';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 function App() {
     return (
         <>
             <Menu />
             <main>
-                <Container>
+                <Container style={{ backgroundColor: "#dedede" }}>
                     <Carousel data-bs-theme="light">
                         <Carousel.Item>
                             {/* <img
@@ -17,29 +20,58 @@ function App() {
                             alt="First slide"
                         /> */}
                             <Image src="dort-gimp-backgroud.png" className="d-block w-100" />
-                            {/* <Carousel.Caption>
-                                <h3>First slide label</h3>
-                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                            </Carousel.Caption> */}
                         </Carousel.Item>
                         <Carousel.Item>
-                        <Image src="dort-ovoce-gimp-backgroud.png" className="d-block w-100" />
-                            {/* <Carousel.Caption>
-                                <h3>Second slide label</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </Carousel.Caption> */}
+                            <Image src="dort-ovoce-gimp-backgroud.png" className="d-block w-100" />
                         </Carousel.Item>
                         <Carousel.Item>
-                        <Image src="tarteletky-gimp-background.png" className="d-block w-100" />
-                            {/* <Carousel.Caption>
-                                <h3>Third slide label</h3>
-                                <p>
-                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                                </p>
-                            </Carousel.Caption> */}
+                            <Image src="tarteletky-gimp-background.png" className="d-block w-100" />
                         </Carousel.Item>
                     </Carousel>
                 </Container>
+                <Container>
+
+                    <Row className="marketing">
+                        <Col lg={4}>
+                            <Image src="dort.png" width={140} height={140} rounded />
+                            <h2 class="fw-normal">Dorty</h2>
+                            <p><Button href="#">Detail</Button></p>
+                        </Col>
+                        <Col lg={4}>
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
+                            <h2 class="fw-normal">Zákusky</h2>
+
+                            <p><Button href="#">Detail</Button></p>
+                        </Col>
+                        <Col lg={4}>
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
+                            <h2 class="fw-normal">Francouzské zákusky</h2>
+
+                            <p><Button href="#">Detail</Button></p>
+                        </Col>
+                    </Row>
+                    <Row className="marketing">
+                        <Col lg={4}>
+                            <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)" /></svg>
+                            <h2 class="fw-normal">Chlebíčky</h2>
+
+                            <p><Button href="#">Detail</Button></p>
+                        </Col>
+                        <Col lg={4}>
+                            <Image src="cukrovi.png" width={140} height={140} roundedCircle />
+                            <h2 class="fw-normal">Cukroví</h2>
+
+                            <p><Button href="#">Detail</Button></p>
+                        </Col>
+                        <Col lg={4}>
+                            <Image src="kolace.png" width={140} height={140} roundedCircle />
+                            <h2 class="fw-normal">Koláče</h2>
+
+                            <p><Button href="#">Detail</Button></p>
+                        </Col>
+                    </Row>
+                </Container>
+
             </main>
         </>
     );
--- src/main/js/rodinne-peceni/src/components/Menu.js
+++ src/main/js/rodinne-peceni/src/components/Menu.js
@@ -2,32 +2,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 
 function Menu() {
     return (
-        <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home">Rodinné pečení</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                    <NavDropdown title="Nabídka" id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#pricing">O nás</Nav.Link>
-                        <Nav.Link href="#pricing">Kontakt</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <header>
+            <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home"><Image src='vojteska.png' fluid alt='rodinne-peceni-vojteska' style={{ with: "30px", height: "30px" }} /> Rodinné pečení</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavDropdown title="Nabídka" id="collapsible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="#pricing">O nás</Nav.Link>
+                            <Nav.Link href="#pricing">Kontakt</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
     );
 }
 
