d4e5c5c7f995d58f87f4525028aeb3b8ce0d4cdf
Dalibor Lis <dalibor.lis@oracle.com>
2024-03-27 15:51:55 +0100

    Add categories

--- src/main/js/rodinne-peceni/src/components/Section.js
+++ src/main/js/rodinne-peceni/src/components/Section.js
@@ -1,18 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 import { sections_data } from "./data.js";
+import { useNavigate } from 'react-router-dom';
 
 function Section(props) {
 
-    const items = sections_data[props.origin].items;
+    const [category, setCategory] = useState(null);
+
+    const navigate = useNavigate();
+
+    const handleCategory = category => {
+        setCategory(category);
+    };
 
     useEffect(() => {
         console.log("componentDidMount");
     }, []);
 
-    const renderItems = () => {
+    const renderItems = items => {
         if (items === undefined) {
             return null;
         }
@@ -23,7 +32,7 @@ function Section(props) {
                     <Row key={index} className='featurette'>
                         <div className={"col-md-7" + (index % 2 === 0 ? "" : " order-md-2")}>
                             <h5 className="featurette-heading fw-normal lh-1">{item.name}<span className="text-body-secondary">{item.secondary}</span></h5>
-                            <p className="lead">Some great placeholder content for the first featurette here. Imagine some exciting prose here.</p>
+                            <p className="lead"></p>
                         </div>
                         <div className={"col-md-5" + (index % 2 === 0 ? "" : " order-md-1")}>
                             <Image className="featurette-image img-fluid mx-auto" src={item.image} width={500} height={500} />
@@ -34,11 +43,70 @@ function Section(props) {
         );
     };
 
+    const renderCategories = () => {
+        const categories = sections_data[props.origin].categories;
+        const keys = Object.keys(categories);
+        const rows = keys.reduce((accumulator, key, index) => {
+            if (index % 3 === 0) { //render 3 columns per row
+                const stepKeys = keys.slice(index, index + 3);
+                const columns = stepKeys.map(stepKey => {
+                    const categoryDetail = categories[stepKey];
+                    return (
+                        <Col lg={4}>
+                            <Image src={categoryDetail.image} width={140} height={140} rounded />
+                            <h2 class="fw-normal">{stepKey}</h2>
+                            <p><Button onClick={() => handleCategory(stepKey)}>Detail</Button></p>
+                        </Col>
+                    );
+                });
+                accumulator.push(
+                    <Row className="marketing" style={{ justifyContent: "center" }}>
+                        {columns}
+                    </Row>
+                );
+            }
+            return accumulator;
+        }, []);
+
+        return (
+            <Container style={{ marginTop: "3em" }}>
+                {rows}
+            </Container>
+        );
+    };
+
+    const renderContent = () => {
+        if (Object.keys(sections_data[props.origin].categories).length > 1) {
+            if (category !== null) { // render items for selected category
+                const items = sections_data[props.origin].categories[category].items;
+                return (
+                    renderItems(items)
+                );
+            } else { // render categories
+                return (
+                    renderCategories()
+                );
+            }
+        } else if (Object.keys(sections_data[props.origin].categories).length === 1) { // only one category exists, render category items
+            const firstKey = Object.keys(sections_data[props.origin].categories)[0];
+            const items = sections_data[props.origin].categories[firstKey].items;
+            return (
+                renderItems(items)
+            );
+        } else { // no categories - no items - render nothing
+            return null;
+        }
+    };
+
     return (
         <Container className='marketing'>
-            <h1 className="display-2">{sections_data[props.origin].title}</h1>
+            <h1 className="display-4">
+                <span style={{ cursor: "pointer" }} onClick={() => navigate("/")}>Nabídka</span>
+                {<span style={{ cursor: "pointer" }} onClick={() => handleCategory(null)}> - {sections_data[props.origin].title}</span>}
+                {category && (<span> - {category} </span>)}
+            </h1>
             {
-                renderItems()
+                renderContent()
             }
         </Container>
     );
--- src/main/js/rodinne-peceni/src/components/data.js
+++ src/main/js/rodinne-peceni/src/components/data.js
@@ -1,6 +1,31 @@
 export const sections_data = {
     "dorty": {
         title: "Dorty",
+        categories: {
+            "Dětské": {
+                image: "images/dort.png",
+                items: [
+                    { name: "Dort potahovaný. ", secondary: "Lesní motiv.", image: "images/dort.png" },
+                    { name: "Dort ovocný. ", secondary: "Lesní ovoce.", image: "images/dort-ovoce.png" }
+                ]
+            },
+            "Krémové": {
+                image: "images/dort.png",
+                items: []
+            },
+            "Ovocné": {
+                image: "images/dort.png",
+                items: []
+            },
+            "Potažené": {
+                image: "images/dort.png",
+                items: []
+            },
+            "S jedlým papírem": {
+                image: "images/dort.png",
+                items: []
+            }
+        },
         items: [
             { name: "Dort potahovaný. ", secondary: "Lesní motiv.", image: "images/dort.png" },
             { name: "Dort ovocný. ", secondary: "Lesní ovoce.", image: "images/dort-ovoce.png" }
@@ -8,22 +33,27 @@ export const sections_data = {
     },
     "zakusky": {
         title: "Zákusky",
+        categories: {},
         items: []
     },
     "fr_zakusky": {
         title: "Francouzské zákusky",
+        categories: {},
         items: []
     },
     "chlebicky": {
         title: "Chlebíčky",
+        categories: {},
         items: []
     },
     "cukrovi": {
         title: "Cukroví",
+        categories: {},
         items: []
     },
     "kolace": {
         title: "Koláče",
+        categories: {},
         items: []
     }
 };
