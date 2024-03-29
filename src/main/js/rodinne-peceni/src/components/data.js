444eaa45c299e7b50d8e5763bb16f5c19b1a6568
Dalibor Lis <dalibor.lis@oracle.com>
2024-03-29 21:55:50 +0100

    Use tiny version imges in cakes

--- src/main/js/rodinne-peceni/src/components/data.js
+++ src/main/js/rodinne-peceni/src/components/data.js
@@ -5,8 +5,8 @@ export const sections_data = {
             "Dětské": {
                 image: "images/dort-detsky-menu-tiny.png",
                 items: [
-                    { name: "Dort potahovaný. ", secondary: "Lesní motiv.", image: "images/dort.png" },
-                    { name: "Dort ovocný. ", secondary: "Lesní ovoce.", image: "images/dort-ovoce.png" }
+                    { name: "Dort potahovaný. ", secondary: "Lesní motiv.", image: "images/dort-tiny.png" },
+                    { name: "Dort ovocný. ", secondary: "Lesní ovoce.", image: "images/dort-ovoce-tiny.png" }
                 ]
             },
             "Krémové": {
