16ef377cb63c47bab255c5a85437de5ab0bae875
Dalibor Lis <dalibor.lis@oracle.com>
2024-04-28 13:34:24 +0200

    Custom routing service prototype

--- src/main/java/lis/rp/SOAPageResource.java
+++ src/main/java/lis/rp/SOAPageResource.java
@@ -1,5 +1,11 @@
 package lis.rp;
 
+import io.helidon.microprofile.server.RoutingName;
+import io.helidon.microprofile.server.RoutingPath;
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.Service;
+import io.helidon.webserver.staticcontent.StaticContentSupport;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -19,27 +25,56 @@ import java.util.stream.Collectors;
  * The purpose of the class is to handle UI navigation requests sent to server.
  * By default, the navigation is handled by React router navigation framework.
  */
-@Path("/{a:dorty|zakusky|fr_zakusky|chlebicky|kolace|cukrovi|onas|cenik|kontakt}")
-public class SOAPageResource {
+@ApplicationScoped
+@RoutingName(value = "@default", required = true)
+@RoutingPath("") // In Helidon 4 routing path should be /
+public class SOAPageResource implements Service {
 
-    private final String soaIndex;
-    private final SSRService ssrService;
+//    private final String soaIndex;
+//
+//    @Inject
+//    public SOAPageResource(@ConfigProperty(name = "soa.index") String soaIndex) {
+//        this.soaIndex = soaIndex;
+//    }
+//
+//    @GET
+//    @Produces(MediaType.TEXT_HTML)
+//    public Response uiNavigation() throws IOException {
+//        System.out.println("Here");
+//        String content;
+//        try (var is = new BufferedReader(new InputStreamReader(Objects.requireNonNull(SOAPageResource.class.getClassLoader().getResourceAsStream(soaIndex)), StandardCharsets.UTF_8))) {
+//            content = is.lines().collect(Collectors.joining());
+//        }
+//        return Response.ok(content).build();
+//    }
 
-    @Inject
-    public SOAPageResource(@ConfigProperty(name = "soa.index") String soaIndex, SSRService ssrService) {
-        this.soaIndex = soaIndex;
-        this.ssrService = ssrService;
+      // Helidon 4 version
+//    @Override
+//    public void routing(HttpRules httpRules) {
+//        httpRules.get("", (req, res) -> {
+//            String content;
+//            try (var is = new BufferedReader(new InputStreamReader(Objects.requireNonNull(SOAPageResource.class.getClassLoader().getResourceAsStream("web/index.html")), StandardCharsets.UTF_8))) {
+//                content = is.lines().collect(Collectors.joining());
+//            }
+//            res.header("Content-Type", "text/html");
+//            res.send(content);
+//        });
+//        httpRules.register("", StaticContentService.builder("web").build());
+//    }
+
+    @Override
+    public void update(Routing.Rules httpRules) {
+        httpRules.get("/", (req, res) -> {
+            String content;
+            try (var is = new BufferedReader(new InputStreamReader(Objects.requireNonNull(SOAPageResource.class.getClassLoader().getResourceAsStream("web/index.html")), StandardCharsets.UTF_8))) {
+                content = is.lines().collect(Collectors.joining());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            res.addHeader("Content-Type", "text/html");
+            res.send(content);
+        });
+
+        httpRules.register("", StaticContentSupport.builder("web").build());
     }
-
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public Response uiNavigation() throws IOException {
-        System.out.println(ssrService.toString());
-        String content;
-        try (var is = new BufferedReader(new InputStreamReader(Objects.requireNonNull(SOAPageResource.class.getClassLoader().getResourceAsStream(soaIndex)), StandardCharsets.UTF_8))) {
-            content = is.lines().collect(Collectors.joining());
-        }
-        return Response.ok(content).build();
-    }
-
 }
