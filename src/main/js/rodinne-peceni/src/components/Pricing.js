a338456e085ec2d2e5176605ef42c88b52e9ff39
Dalibor Lis <dalibor.lis@oracle.com>
2024-08-20 14:47:17 +0200

    Add pricing data.

--- src/main/js/rodinne-peceni/src/components/Pricing.js
+++ src/main/js/rodinne-peceni/src/components/Pricing.js
@@ -1,9 +1,15 @@
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 function Pricing() {
     return (
-        <Container>
-            <h1 className='display-4'>Bude brzy doplněno</h1>
+        <Container className='text-center'>
+            <h1 className='display-4 text-center'>Ceník</h1>
+            <object data="cenik.pdf" type="application/pdf" width="100%" height="800px">
+                <p>
+                    <Button href='cenik.pdf'>Stáhnout ceník</Button>
+                </p>
+            </object>
         </Container>
     );
 }
